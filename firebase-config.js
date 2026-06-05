9189db1307b58e70e2e25c3efd99b7274eb149ac
JadzAdCo <bans.don@gmail.com>
2026-06-05 01:08:17 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB68VIfHGE-HicL0L8pocXq3pdK7LHBW_g",
+    authDomain: "shoutoutdemo-5b402.firebaseapp.com",
+    projectId: "shoutoutdemo-5b402",
+    storageBucket: "shoutoutdemo-5b402.firebasestorage.app",
+    messagingSenderId: "858205816285",
+    appId: "1:858205816285:web:d5c1ea010001122ecfa234",
+    measurementId: "G-9VZ3SJYHC9"
+  }
